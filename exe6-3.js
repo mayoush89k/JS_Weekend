cb3583d74b0e2cc2c2d43d6217682d7a89034dee
mayoush89k <43721924+mayoush89k@users.noreply.github.com>
2023-09-24 08:39:42 +0300

    updating 6 - 3

--- exe6-3.js
+++ exe6-3.js
@@ -1,10 +1,17 @@
 function OrganizeStrings(word1, word2) {
-  let result = "";
-  for (let w = 'a'; w < 'z'; w++) {
-    if (word1.includes(w) || word2.includes(w)) {
-      result += w;
+  let merging = word1 + word2;
+  merging = merging.split("").sort().join("");
+
+  result = merging[0];
+
+  for(let w = 1 ; w < merging.length ; w++){
+      if (merging[w] != merging[w-1]){
+        result += merging[w];
+      } 
     }
-  }
+  
+
   console.log(`a: ${word1} , b: ${word2} -> ${result}`);
 }
-OrganizeStrings("xyaabbbccccdefww", "xxxxyyyyabklmopq")
+OrganizeStrings("xyaabbbccccdefww", "xxxxyyyyabklmopq");
+OrganizeStrings("abcdefghijklmnopqrstuvwxyz")
